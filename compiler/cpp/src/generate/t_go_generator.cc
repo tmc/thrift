0befc27012743a98d962db6dbc01667b93cf5443
Travis Cline <travis.cline@gmail.com>
2013-05-27 21:35:20 -0700

    Perform dummy math assignment in generated -remote files as well.

--- compiler/cpp/src/generate/t_go_generator.cc
+++ compiler/cpp/src/generate/t_go_generator.cc
@@ -1875,6 +1875,7 @@ void t_go_generator::generate_service_remote(t_service* tservice)
              indent() << "var help bool" << endl <<
              indent() << "var parsedUrl url.URL" << endl <<
              indent() << "var trans thrift.TTransport" << endl <<
+             indent() << "_ = math.MinInt32" << endl <<
              indent() << "flag.Usage = Usage" << endl <<
              indent() << "flag.StringVar(&host, \"h\", \"localhost\", \"Specify host and port\")" << endl <<
              indent() << "flag.IntVar(&port, \"p\", 9090, \"Specify port\")" << endl <<
