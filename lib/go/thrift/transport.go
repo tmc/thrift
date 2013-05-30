5d9b872c818a3fa05b0f6fcb30b5e7f6ab45b798
Travis Cline <travis.cline@gmail.com>
2013-05-30 11:26:51 -0700

    TTransport simplification and cleanup.

--- lib/go/thrift/transport.go
+++ lib/go/thrift/transport.go
@@ -20,8 +20,6 @@
 package thrift
 
 import (
-	"log"
-	"os"
 	"strconv"
 )
 
@@ -29,67 +27,14 @@ type Flusher interface {
 	Flush() (err error)
 }
 
-/**
- * Generic class that encapsulates the I/O layer. This is basically a thin
- * wrapper around the combined functionality of Java input/output streams.
- *
- */
+// Encapsulates the I/O layer
 type TTransport interface {
-	/**
-	 * Queries whether the transport is open.
-	 *
-	 * @return True if the transport is open.
-	 */
 	IsOpen() bool
-
-	/**
-	 * Opens the transport for reading/writing.
-	 *
-	 * @returns TTransportException if the transport could not be opened
-	 */
 	Open() (err error)
-
-	/**
-	 * Closes the transport.
-	 */
 	Close() (err error)
-
-	/**
-	 * Reads up to len bytes into buffer buf, starting att offset off.
-	 *
-	 * @param buf Array to read into
-	 * @param off Index to start reading at
-	 * @param len Maximum number of bytes to read
-	 * @return The number of bytes actually read
-	 * @return TTransportException if there was an error reading data
-	 */
 	Read(buf []byte) (n int, err error)
-
-	/**
-	 * Guarantees that all of len bytes are actually read off the transport.
-	 *
-	 * @param buf Array to read into
-	 * @param off Index to start reading at
-	 * @param len Maximum number of bytes to read
-	 * @return The number of bytes actually read, which must be equal to len
-	 * @return TTransportException if there was an error reading data
-	 */
 	ReadAll(buf []byte) (n int, err error)
-
-	/**
-	 * Writes the buffer to the output
-	 *
-	 * @param buf The output data buffer
-	 * @return Number of bytes written
-	 * @return TTransportException if an error occurs writing data
-	 */
 	Write(buf []byte) (n int, err error)
-
-	/**
-	 * Flush any pending data out of a transport buffer.
-	 *
-	 * @return TTransportException if there was an error writing out data.
-	 */
 	Flush() (err error)
 
 	/**
@@ -148,15 +93,8 @@ func (p* TTransportBase) Flush() error {
   return nil;
 }
 */
-/**
- * Guarantees that all of len bytes are actually read off the transport.
- *
- * @param buf Array to read into
- * @param off Index to start reading at
- * @param len Maximum number of bytes to read
- * @return The number of bytes actually read, which must be equal to len
- * @return TTransportException if there was an error reading data
- */
+
+// Reads all the bytes off the transport. Returns the umber of bytes read and an error
 func ReadAllTransport(p TTransport, buf []byte) (n int, err error) {
 	ret := 0
 	size := len(buf)
@@ -171,12 +109,4 @@ func ReadAllTransport(p TTransport, buf []byte) (n int, err error) {
 		n += ret
 	}
 	return n, err
-}
-
-var (
-	LOGGER *log.Logger
-)
-
-func init() {
-	LOGGER = log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
